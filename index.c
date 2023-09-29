1f8d23350ae9fdbbe1e6fa3dbe025280fcf7bc63
Krishna Kamdar <krishnakamdar90@gmail.com>
2023-09-29 16:35:58 +0530

    print numebr 5

--- index.c
+++ index.c
@@ -1,12 +1,6 @@
 #include <stdio.h>
 
 int main(){
-    int x = 5;
-    printf("%d", x);
-<<<<<<< HEAD
-    printf("this is new feature34");
-=======
-    printf("this is new feature23");
->>>>>>> main
+    printf("5");
     return 0;
 }
